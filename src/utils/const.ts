4e42f6babaf17029c3fe1d19fd640a2ad4e32eed
laomai <85533115+laomaiorg@users.noreply.github.com>
2024-09-04 12:28:50 +0800

    feat: 获取个人数据

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -37,7 +37,7 @@ const LINE_OPACITY = 0.6;
 // styling: map height
 const MAP_HEIGHT = 600;
 //set to `false` if you want to hide the road label characters
-const ROAD_LABEL_DISPLAY = true;
+const ROAD_LABEL_DISPLAY = false;
 //set to `true` if you want to display only the routes without showing the map.
 const PRIVACY_MODE = false;
 //set to `false` if you want to make light off as default, only effect when `PRIVACY_MODE` = false

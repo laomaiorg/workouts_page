8d0b81df25d9aef80fd8cb8d88ef5a7a1c9a4a79
laomai <85533115+laomaiorg@users.noreply.github.com>
2024-09-06 11:05:49 +0800

    feat: change my mapbox token and use keep to strava

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ const MAPBOX_TOKEN =
     // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoiYmVuLTI5IiwiYSI6ImNrZ3Q4Ym9mMDBqMGYyeXFvODV2dWl6YzQifQ.gSKoWF-fMjhzU67TuDezJQ';
+  'pk.eyJ1IjoibGFvbWFpIiwiYSI6ImNtMG0xaG01azBjYTgyaXB5MWxyOTZ6bDUifQ.mLJBaKg2-kh46wUvqxMUew';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
